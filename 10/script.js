151d88fdf8cebb1838d5882cb8cf34faf96a8bb5
Bampus <65647017+Bampus@users.noreply.github.com>
2020-10-19 14:24:09 +0200

    Uppgift 10

---
+++ 10/script.js
@@ -0,0 +1,6 @@
+function delbartMed3(array) {
+    array [3, 6, 7, 9]
+    if(array % == 3)
+    return array
+}
+// Jag kommer få plugga in det här trodde jag kunde när jag testade mig förut men det verkar inte ha fastnat.
